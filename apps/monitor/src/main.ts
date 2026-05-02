d91c5f10ac4b6608ff80e7bde207a50a65205496
Pritam Biswas <pritambiswas060@gmail.com>
2026-05-02 16:22:16 +0000

    feat: add global validation pipe to the main application and refactor user controller methods for consistency

--- apps/monitor/src/main.ts
+++ apps/monitor/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
   const config = new DocumentBuilder()
   .setTitle("Monitor Server")
   .setDescription('The Monitor Server API Docs')
--- apps/monitor/src/users/users.controller.ts
+++ apps/monitor/src/users/users.controller.ts
@@ -2,30 +2,29 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { isoUserId, userIdPipeTransformer, type UserId } from './entities/user.entity';
+import { userIdPipeTransformer, type UserId } from './entities/user.entity';
 import { pipe } from 'fp-ts/function';
-import * as TE from 'fp-ts/TaskEither';
 import * as E from 'fp-ts/Either';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(private readonly usersService: UsersService) { }
 
   @Post()
   async create(@Body() createUserDto: CreateUserDto) {
-      const result = await this.usersService.create(createUserDto)();
-      return pipe(
-         result,
-         E.getOrElseW((error) => { throw error })
-      )
+    const result = await this.usersService.create(createUserDto)();
+    return pipe(
+      result,
+      E.getOrElseW((error) => { throw error })
+    )
   }
 
   @Get()
   async findAll() {
     const result = await this.usersService.findAll()();
     return pipe(
-       result,
-       E.getOrElseW((error) => { throw error })
+      result,
+      E.getOrElseW((error) => { throw error })
     )
   }
 
@@ -33,8 +32,8 @@ export class UsersController {
   async findOne(@Param('id', userIdPipeTransformer) id: UserId) {
     const result = await this.usersService.findOne(id)();
     return pipe(
-       result,
-       E.getOrElseW((error) => { throw error })
+      result,
+      E.getOrElseW((error) => { throw error })
     )
   }
 
@@ -42,17 +41,17 @@ export class UsersController {
   async update(@Param('id', userIdPipeTransformer) id: UserId, @Body() updateUserDto: UpdateUserDto) {
     const result = await this.usersService.update(id, updateUserDto)();
     return pipe(
-       result,
-       E.getOrElseW((error) => { throw error })
+      result,
+      E.getOrElseW((error) => { throw error })
     )
   }
 
   @Delete(':id')
   async remove(@Param('id', userIdPipeTransformer) id: UserId) {
-     const result = await this.usersService.remove(id)();
-     return pipe(
-       result,
-       E.getOrElseW((error) => { throw error })
-     ) 
+    const result = await this.usersService.remove(id)();
+    return pipe(
+      result,
+      E.getOrElseW((error) => { throw error })
+    )
   }
 }
